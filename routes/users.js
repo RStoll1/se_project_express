c247490c2e4a5a38652a9d686a76545fba65fa30
morti3l <pushthatboard@gmail.com>
2025-12-04 02:27:43 -0600

    See react repo for changes.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getUsers, getCurrentUser, updateCurrentUser } = require('../controllers/users');
+const { getCurrentUser, updateCurrentUser } = require('../controllers/users');
 
 router.get('/me', getCurrentUser);
 router.patch('/me', updateCurrentUser);
